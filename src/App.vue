9754e7c6ee072203a94ca91a645f043cfc795da0
mutua <mutuamutinda@outlook.com>
2022-06-20 12:42:20 +0300

    complete ui

--- src/App.vue
+++ src/App.vue
@@ -2,3 +2,21 @@
   <router-view/>
 </template>
 
+<script>
+import { mapGetters } from 'vuex';
+export default {
+   beforeMount() {
+    this.$store.dispatch("initTheme");
+  },
+  computed: {
+    ...mapGetters({ theme: "getTheme" }),
+  },
+    watch: {
+    theme(newTheme) {
+      newTheme === "light"
+        ? document.querySelector("html").classList.remove("dark")
+        : document.querySelector("html").classList.add("dark");
+    },
+  },
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '3xl': '-5px -5px 10px #FFFFFF, 5px 5px 10px rgba(55, 84, 170, 0.4)',
+        '4xl': '5px 5px 10px #000000, -5px -5px 10px rgba(20, 21, 24, 0.4)',
+        '5xl':'-5px -5px 10px #FFFFFF, 5px 5px 10px rgba(55, 84, 170, 0.1)',
+        '6xl': '5px 5px 10px #000000, -5px -5px 10px rgba(20, 21, 24, 0.1)',
+        '7xl': '-5px -5px 10px #FFFFFF, 5px 5px 10px rgba(21, 101, 192, 0.4)'
+
+      },
+      backgroundImage: {
+        'dark-gradient': "linear-gradient(180deg, #1A1B1F 0%, #17181C 100%);",
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
