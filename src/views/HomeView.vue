3375a1ef087bd12fc169e9d02b4d3cc661d05c18
mutua <mutuamutinda@outlook.com>
2022-06-20 22:55:00 +0300

    basic computation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,39 +25,39 @@
         <!-- screen -->
         <div class="mt-3 shadow-5xl rounded-2xl w-full p-2 bg-[#F1F3F6] dark:shadow-6xl dark:bg-[#141518]">
           <label for="screen" class="sr-only">calculation</label>
-          <input type="" name="screen" id="screen" :value="enteredVal" placeholder="88888888" class="p-8 w-full text-4xl text-right text-gray-700 border-2 border-[#1565C0] rounded-md outline-none dark:text-gray-300 dark:bg-[#202226]">
+          <input type="text" name="screen" id="screen" v-model="enteredVal" placeholder="88888888" class="p-8 w-full text-4xl text-right text-gray-700 border-2 border-[#1565C0] rounded-md outline-none dark:text-gray-300 dark:bg-[#202226]" />
         </div>
 
         <!-- keypad -->
         <main class="mt-10">
           <div class="grid grid-cols-4 gap-x-3">
-            <button value="AC" @click="enteredVal = []" class="button-value">AC</button>
+            <button value="AC" @click="clearAll" class="button-value">AC</button>
             <button value="C"  @click="clear" class="button-value">C</button>
-            <button value="%"  @click="getVal($event)" class="button-value">%</button>
-            <button value="/"  @click="getVal($event)" class="button-ops">/</button>
+            <button value="%"  @click="percent" class="button-value">%</button>
+            <button value="/"  @click="divide" class="button-ops">/</button>
           </div>
          <div class="mt-4 grid grid-cols-4 gap-x-3">
-            <button value="7"  @click="getVal($event)" class="button-value">7</button>
-            <button value="8"  @click="getVal($event)" class="button-value">8</button>
-            <button value="9"  @click="getVal($event)" class="button-value">9</button>
-            <button value="*"  @click="getVal($event)" class="button-ops">*</button>
+            <button value="7"  @click="keyVal('7')" class="button-value">7</button>
+            <button value="8"  @click="keyVal('8')" class="button-value">8</button>
+            <button value="9"  @click="keyVal('9')" class="button-value">9</button>
+            <button value="*"  @click="multiply" class="button-ops">*</button>
           </div>
           <div class="mt-4 grid grid-cols-4 gap-x-3">
-            <button value="4"  @click="getVal($event)" class="button-value">4</button>
-            <button value="5"  @click="getVal($event)" class="button-value">5</button>
-            <button value="6"  @click="getVal($event)" class="button-value">6</button>
-            <button value="-"  @click="getVal($event)" class="button-ops">-</button>
+            <button value="4"  @click="keyVal('4')" class="button-value">4</button>
+            <button value="5"  @click="keyVal('5')" class="button-value">5</button>
+            <button value="6"  @click="keyVal('6')" class="button-value">6</button>
+            <button value="-"  @click="substract" class="button-ops">-</button>
           </div>
           <div class="mt-4 grid grid-cols-4 gap-x-3">
-            <button value="3"  @click="getVal($event)" class="button-value">3</button>
-            <button value="2"  @click="getVal($event)" class="button-value">2</button>
-            <button value="1"  @click="getVal($event)" class="button-value">1</button>
-            <button value="+"  @click="getVal($event)" class="button-ops">+</button>
+            <button value="3"  @click="keyVal('3')" class="button-value">3</button>
+            <button value="2"  @click="keyVal('2')" class="button-value">2</button>
+            <button value="1"  @click="keyVal('1')" class="button-value">1</button>
+            <button value="+"  @click="add" class="button-ops">+</button>
           </div>
          <div class="mt-4 grid grid-cols-4 gap-x-3">
-            <button value="0"  @click="getVal($event)" class="col-span-2 p-5 h-20 w-full button-value">0</button>
-            <button value="."  @click="getVal($event)" class="button-value">.</button>
-            <button value="="  @click="compute" class="button-ops bg-[#1565C0] shadow-7xl text-gray-200 dark:bg-[#1565C0]">=</button>
+            <button value="0"  @click="keyVal('0')" class="col-span-2 p-5 h-20 w-full button-value">0</button>
+            <button value="."  @click="dot" class="button-value">.</button>
+            <button value="="  @click="compute" class=" button-ops bg-[#1565C0] shadow-7xl text-gray-200 dark:bg-[#1565C0]">=</button>
           </div>
         </main>
 
@@ -71,23 +71,73 @@ export default {
   name: 'HomeView',
 
   setup(){
-    const enteredVal = ref([])
+    const enteredVal = ref('')
+    const previous = ref(null)
+    const operator = ref(null)
+    const operatorClicked = ref(false)
+
     return {
-      enteredVal
+      enteredVal,
+      previous,
+      operator,
+      operatorClicked,
+      
     }
   },
-  
     methods: {
-      getVal(e){
-        const currentVal = e.target.value
-          this.enteredVal.push(currentVal)
+      clear() {
+        this.enteredVal = this.enteredVal.slice(0, -1);
       },
-      clear(){
-        if(this.enteredVal !== []) {
-          this.enteredVal.pop()
+      clearAll() {
+        this.enteredVal = '';
+      },
+      percent(){
+        this.enteredVal = `${parseFloat(this.enteredVal)/100}`
+      },
+      keyVal(number){
+        if (this.operatorClicked) {
+          this.enteredVal = '';
+          this.operatorClicked = false;
+        }
+        this.enteredVal = `${this.enteredVal}${number}`
+      },
+      dot(){
+        if (this.enteredVal.indexOf('.') === -1) {
+          this.keyVal('.');
         }
       },
-  
+      setPrevious() {
+        this.previous = this.enteredVal;
+        this.operatorClicked = true;
+      },
+
+      divide() {
+        this.operator = (a, b) => a / b;
+        this.setPrevious();
+      },
+
+      multiply() {
+        this.operator = (a, b) => a * b;
+        this.setPrevious();
+      },
+
+      substract() {
+        this.operator = (a, b) => b - a;
+        this.setPrevious();
+      },
+
+      add() {
+        this.operator = (a, b) => a + b;
+        this.setPrevious();
+      },
+      compute() {
+        this.enteredVal = `${this.operator(
+          parseFloat(this.enteredVal), 
+          parseFloat(this.previous)
+        )}`;
+        this.previous = null;
+    }, 
+
     toggleTheme() {
       this.$store.dispatch("toggleTheme");
     },
